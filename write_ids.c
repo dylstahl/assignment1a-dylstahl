1766053d387ace699ee23a6f756e8dca0e1c91df
Dylan Stahl <dfs5886@psu.edu>
2024-09-16 18:29:36 -0400

    Id has been changed

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "dylstahl";
+    char STUDENT_ID[] = "975617365";
 
     return 0 ;
 }
